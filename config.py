640f3dca95ac486774b31ecdd3e7677e4939d2c7
env0id <denbaranic@gmail.com>
2026-02-03 20:25:57 +0100

    +updated config

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID_LIST = os.getenv('ADMIN_ID_LIST')
+ADMIN_ID_LIST = ast.literal_eval(os.getenv('ADMIN_ID_LIST'))
 DB_NAME = os.getenv('DB_NAME')
-ADMIN_ID_LIST = ast.literal_eval(ADMIN_ID_LIST)
